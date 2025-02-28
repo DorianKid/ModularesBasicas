55a5d7abb08c268f2ad9f611e86cc35fb39f1834
Dario Abraham Curiel Niño <130793949+DorianKid@users.noreply.github.com>
2025-02-28 16:26:16 -0600

    Create Modulares.py

---
+++ Modulares.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Modulares",
+    page_icon=":microscope:",
+    layout="wide",    
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': "# This is a header. This is an *extremely* cool app!"}
+)
+
+st.write("# ¡Bienvenido! ")
+
+st.markdown(
+"""
+"""
+)
