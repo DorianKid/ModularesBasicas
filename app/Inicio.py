fd1b33a7d5480d8fd887a46d3b86d3f7d17b18eb
Dario Abraham Curiel Niño <130793949+DorianKid@users.noreply.github.com>
2025-03-03 06:17:14 -0600

    Update Inicio.py

--- app/Inicio.py
+++ app/Inicio.py
@@ -14,7 +14,7 @@ st.set_page_config(
 
 st.title("Proyectos Modulares: Evaluación Integral en CUCEI")
 st.markdown("""
-### Los Proyectos Modulares constituyen una estrategia educativa fundamental dentro del modelo académico del Centro Universitario de 
+### Los Proyectos Modulares constituyen una estrategia educativa fundamental dentro del modelo académico del Centro Universitario de... 
 Ciencias Exactas e Ingenierías (CUCEI) de la Universidad de Guadalajara. Estos proyectos representan un sistema de evaluación 
 integral que permite a los estudiantes demostrar la adquisición y aplicación de competencias específicas en diferentes etapas de su formación profesional.
 """)
