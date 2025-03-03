4d404f286e37aa2d5cba7e91089f4917a17b50c7
Dario Abraham Curiel Niño <130793949+DorianKid@users.noreply.github.com>
2025-03-03 10:56:30 -0600

    Update Inicio.py

--- app/Inicio.py
+++ app/Inicio.py
@@ -182,7 +182,7 @@ else:
 with st.expander('Ver plantilla', expanded=False):
     col1, col2 = st.columns([1,2])
     with col1:
-        contenido_tex = read_tex_file("/mount/src/modularesbasicas/app/files/Plantilla_Modulares-tex")
+        contenido_tex = read_tex_file("/mount/src/modularesbasicas/app/files/Plantilla_Modulares.tex")
         st.write(conetido_tex)
 
 
