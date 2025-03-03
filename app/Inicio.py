85ed9d8c5e05fd59a24e52f4ff0b38c3c9d0a9ff
Dario Abraham Curiel Niño <130793949+DorianKid@users.noreply.github.com>
2025-03-03 05:51:14 -0600

    Update Inicio.py

--- app/Inicio.py
+++ app/Inicio.py
@@ -3,7 +3,7 @@ import base64
 
 st.set_page_config(
     page_title="Modulares",
-    page_icon=":memo:",
+    page_icon=":pencil    :",
     layout="wide",    
     initial_sidebar_state="expanded",
     menu_items={
