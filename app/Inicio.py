bb7804e51438ae38eee51ad468bef2f1f7640699
Dario Abraham Curiel Niño <130793949+DorianKid@users.noreply.github.com>
2025-03-03 09:14:40 -0600

    Update Inicio.py

--- app/Inicio.py
+++ app/Inicio.py
@@ -22,7 +22,7 @@ st.markdown(
         background-color: #4CAF50;  /* Color de fondo */
         color: white;  /* Color del texto */
         border: none;  /* Sin borde */
-        padding: 10px 20px;  /* Espaciado interno */
+        padding: 10px 15px;  /* Espaciado interno */
         text-align: center;  /* Alinear texto */
         text-decoration: none;  /* Sin subrayado */
         display: inline-block;  /* Mostrar en línea */
