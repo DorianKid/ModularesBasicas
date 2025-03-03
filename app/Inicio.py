866df4d8578fd8ea276f71093391bcfb67b70033
Dario Abraham Curiel Niño <130793949+DorianKid@users.noreply.github.com>
2025-03-03 12:00:17 -0600

    Update Inicio.py

--- app/Inicio.py
+++ app/Inicio.py
@@ -205,7 +205,7 @@ with st.expander("Ver Plantilla", expanded=True):
                 {
                 "page": 1,
                 "x": 0,
-                "y": 790,
+                "y": 792,
                 "height": .5,
                 "width": 595,
                 "color": "red"
